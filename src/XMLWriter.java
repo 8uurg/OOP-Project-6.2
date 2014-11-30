7425c771816ba9eb44c3ceef75162ffafdcc7075
8uurg <arthur.guijt12@gmail.com>
2014-11-30 18:17:30 +0100

    XML Writer demo.

--- src/XMLWriter.java
+++ src/XMLWriter.java
@@ -1,8 +1,10 @@
 import java.io.File;
+import java.util.ArrayList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
@@ -13,6 +15,9 @@ import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+
+import exceptions.TransferException;
+
 
 /**
  * Hulpklasse om een XML Bestand in te laden
@@ -25,9 +30,34 @@ public class XMLWriter {
 			DocumentBuilder docbuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
 			Document doc = docbuilder.newDocument();
 			
+			// Start DEMO Code.
+			ArrayList<Team> teams = new ArrayList<Team>();
 			
+			try {
+				Team a = new Team("FF");
+				a.voegToe(new Speler("John Doe", 12, 120000, Speler.Status.Beschikbaar, Speler.Type.Aanvaller, 70, 20, 60), 0);
+				a.voegToe(new Speler("James Doe", 12, 120000, Speler.Status.Beschikbaar, Speler.Type.Aanvaller, 70, 20, 60), 0);
+				
+				Team b = new Team("VV");
+				b.voegToe(new Speler("Jeffrey Dest", 12, 120000, Speler.Status.Beschikbaar, Speler.Type.Aanvaller, 70, 20, 60), 0);
+				b.voegToe(new Speler("John Dös", 12, 120000, Speler.Status.Beschikbaar, Speler.Type.Verdediger, 30, 70, 60), 0);
+				
+				teams.add(a);
+				teams.add(b);
+			} catch(TransferException e) {
+			}
+			
+			
+			Element teamstag = doc.createElement("teams");
+			
+			for(Team team: teams)
+				teamstag.appendChild(team.getXMLElement(doc));
+			
+			doc.appendChild(teamstag);
 			
 			Transformer transformer = TransformerFactory.newInstance().newTransformer();
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
 			DOMSource doms = new DOMSource(doc);
 			
 			// TODO vervang out.xml
