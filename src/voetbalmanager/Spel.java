b6b24cf80c93bea0aef1abe19417be5cf01e425d
8uurg <arthur.guijt12@gmail.com>
2014-12-06 23:41:41 +0100

    Hoofdklasse voor spel toegevoegd. Nog niets geïmplementeerd.

---
+++ src/voetbalmanager/Spel.java
@@ -0,0 +1,33 @@
+package voetbalmanager;
+
+import java.util.List;
+
+import voetbalmanager.model.Competitie;
+
+public class Spel {
+	
+	public static Spel huidigSpel;
+	
+	public Competitie huidigeCompetitie;
+	
+	public static void main(String[] args) {
+		// TODO Start spel op.
+	}
+	
+	public static void laadSpel(Spel spel)
+	{
+		// TODO Laad een spel met een bepaalde in.
+	}
+	
+	public static void saveHuidigSpel()
+	{
+		// TODO Sla huidig spel op
+	}
+	
+	public List<Spel> getSpellenLijst()
+	{
+		// TODO geef een lijst met savefiles.
+		return null;
+	}
+	
+}
