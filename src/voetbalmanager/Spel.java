7bbf7931cd92ce68891829308e6e2fc2e3a69bc1
8uurg <arthur.guijt12@gmail.com>
2014-12-08 13:22:34 +0100

    "Hoofdklasse" toegevoegd.

--- src/voetbalmanager/Spel.java
+++ src/voetbalmanager/Spel.java
@@ -1,6 +1,12 @@
 package voetbalmanager;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import voetbalmanager.model.Competitie;
 
@@ -9,6 +15,13 @@ public class Spel {
 	public static Spel huidigSpel;
 	
 	public Competitie huidigeCompetitie;
+	public String spelnaam;
+	
+	private Spel(Competitie competitie, String naam)
+	{
+		this.huidigeCompetitie = competitie;
+		this.spelnaam = naam;
+	}
 	
 	public static void main(String[] args) {
 		// TODO Start spel op.
@@ -16,7 +29,8 @@ public class Spel {
 	
 	public static void laadSpel(Spel spel)
 	{
-		// TODO Laad een spel met een bepaalde in.
+		// TODO Laad een spel in.
+		Spel.huidigSpel = spel;
 	}
 	
 	public static void saveHuidigSpel()
@@ -24,10 +38,31 @@ public class Spel {
 		// TODO Sla huidig spel op
 	}
 	
-	public List<Spel> getSpellenLijst()
+	public static List<Spel> getSpellenLijst()
 	{
+		File[] saves = new File("./saves/").listFiles();
+		// TODO Controle of de folder bestaat.
+		ArrayList<Spel> res = new ArrayList<Spel>();
+		
+		for(File save: saves) {
+			try {
+				Document doc = XMLLoader.getDocument(save);
+				
+				res.add(Spel.uitXML((Element) doc.getElementsByTagName("spel").item(0)));
+				// TODO Laad spel.
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+		
 		// TODO geef een lijst met savefiles.
-		return null;
+		return res;
 	}
 	
+	public static Spel uitXML(Element el){
+		String naam = el.getAttribute("naam");
+		Competitie competitie = Competitie.laadXMLElement(((Element) el.getElementsByTagName("competitie").item(0)));
+		
+		return new Spel(competitie, naam);
+	}
 }
