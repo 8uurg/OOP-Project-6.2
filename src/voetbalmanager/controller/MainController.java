3cdf6dbccb62f3bda8a7adf68a03b66a976eba48
Mayke93 <m.l.kloppenburg@student.tudelft.nl>
2015-01-05 14:27:22 +0100

    nog wat dingen

--- src/voetbalmanager/controller/MainController.java
+++ src/voetbalmanager/controller/MainController.java
@@ -28,12 +28,7 @@ public class MainController implements Initializable{
    @FXML private Button LoadGame;
    @FXML private Button Exit;
    @FXML private Button Help;
-   @FXML private Button Next;
-   @FXML private Button Back;
-   @FXML private Button Back2;
-   @FXML private Button StartManagement;
-   @FXML private TextField TekstVeld;
-
+  
    
    @FXML
    public void handleNewGame(ActionEvent event) throws IOException {
@@ -79,60 +74,7 @@ public class MainController implements Initializable{
 	
     }
    
-   @FXML
-   public void handleBack(ActionEvent event) throws IOException {
-	   	Parent root = FXMLLoader.load(MainController.class.getResource("MainMenu.fxml"));
-		Scene scene = new Scene(root,800,600);
-		Stage ps = new Stage();
-		
-		ps.setScene(scene);
-		ps.show();
-	
-    }
-   
-   @FXML
-   public void handleNext(ActionEvent event) throws IOException {
-	  
-	   String naam = TekstVeld.getText();
-	   
-	   String loc = "./saves/" + naam + ".xml";
-	   
-	   Competitie competitie = XMLLoader.creeerCompetitie("eredivisie");
-	   // TODO geef huidige competitie door aan centrale spelbeheerder.
-	   
-	   XMLWriter.saveCompetitie(competitie, new StreamResult(new File(loc)));
-			   
-	   Parent root = FXMLLoader.load(MainController.class.getResource("NewGameNext.fxml"));
-	   Scene scene = new Scene(root);
-	   Stage ps = new Stage();
-		
-		ps.setScene(scene);
-		ps.show();
-			
-    }
-   
-   @FXML
-   public void handleBack2(ActionEvent event) throws IOException {
-	   	Parent root = FXMLLoader.load(MainController.class.getResource("NewGame.fxml"));
-		Scene scene = new Scene(root,800,600);
-		Stage ps = new Stage();
-		
-		ps.setScene(scene);
-		ps.show();
-	
-    }
-   
-   @FXML
-   public void handleStartManagement(ActionEvent event) throws IOException {
-	   	Parent root = FXMLLoader.load(MainController.class.getResource("ManagementMain.fxml"));
-		Scene scene = new Scene(root,800,600);
-		Stage ps = new Stage();
-		
-		ps.setScene(scene);
-		ps.show();
-	
-    }
-   
+  
   
  
 
