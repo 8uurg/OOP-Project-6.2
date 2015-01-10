28ad63a1b0911723a909a8fcde0bd28012fc7bbc
8uurg <arthur.guijt12@gmail.com>
2015-01-10 23:15:38 +0100

    Begin simulatie

--- src/voetbalmanager/controller/wedstrijdsimulatie/WedstrijdSimulator.java
+++ src/voetbalmanager/controller/wedstrijdsimulatie/WedstrijdSimulator.java
@@ -1,34 +1,82 @@
 package voetbalmanager.controller.wedstrijdsimulatie;
 
+import java.util.Random;
+
 import voetbalmanager.model.Team;
 
 public class WedstrijdSimulator {
+	// Aantal rondes per helft
+	private final int aantalrondes = 45;
 	
+	// De teams die meedoen aan het spel
 	private WedstrijdTeam a;
 	private WedstrijdTeam b;
 	
+	// Heeft A de bal?
+	private boolean balA;
+	/**
+	 * 1: Team A doelgebied
+	 * 2: Tussen midden en doelgebied A in.
+	 * 3: Midden
+	 * 4: Tussen midden en doelgebied B in.
+	 * 5: Team B doelgebied
+	 */
+	private int balpositie = 3;
+	private int rn = 0;
+	
+	private Random rnd;
+	
 	public WedstrijdSimulator(Team a, Team b)
 	{
-		this.a = new WedstrijdTeam(a);
-		this.b = new WedstrijdTeam(b);
+		this.a = new WedstrijdTeam(a, true);
+		this.b = new WedstrijdTeam(b, false);
+		
+		rnd = new Random();
 	}
 	
 	public Resultaat simuleer() {
+		this.balA = true;
 		simuleerHelft();
-		wisselkant();
+		this.balA = false;
 		simuleerHelft();
 		return new Resultaat(a.getScore(), b.getScore());
 	}
 	
 	private void simuleerHelft() {
-		// TODO Simulatiecode hier;
-		// Team a is het team dat begint.
+		this.balpositie = 3;
+		
+		for(rn = 0; rn < aantalrondes; ++rn) {
+			simuleerRonde();
+		}
 	}
 	
-	private void wisselkant() {
-		WedstrijdTeam temp = b;
-		b = a;
-		a = temp;
+	private void simuleerRonde() {
+		float amod = 1+((rnd.nextInt(10))-5)/50;
+		float bmod = 1+((rnd.nextInt(10))-5)/50;
+		
+		float powa = a.getKracht(balpositie, balA);
+		float powb = b.getKracht(6-balpositie, !balA);
+		
+		if(amod*powa > bmod*powb) {
+			if(balA){
+				++balpositie;
+			}
+			else
+			{
+				balA = true;
+			}
+		} else {
+			if(!balA) {
+				--balpositie;
+			} else {
+				balA = false;
+			}
+		}
+		
+		if(rnd.nextFloat()>0.9) {
+			// Doelpoging
+		}
+		
 	}
 	
 }
--- src/voetbalmanager/controller/wedstrijdsimulatie/WedstrijdTeam.java
+++ src/voetbalmanager/controller/wedstrijdsimulatie/WedstrijdTeam.java
@@ -1,5 +1,6 @@
 package voetbalmanager.controller.wedstrijdsimulatie;
 
+import voetbalmanager.model.Opstelling;
 import voetbalmanager.model.Team;
 
 /**
@@ -7,10 +8,22 @@ import voetbalmanager.model.Team;
  */
 public class WedstrijdTeam {
 	private int score = 0;
+	private boolean x;
 	private Team team;
 	
-	public WedstrijdTeam(Team team) {
+	//Aanvalskracht aanval
+	private SomKrachten aanval;
+	private SomKrachten midden;
+	private SomKrachten verdediging;
+	
+	public WedstrijdTeam(Team team, boolean x) {
 		this.team = team;
+		this.x = x;
+		
+		Opstelling o = this.team.getOpstelling();
+		aanval = o.getAanvallersKrachten();
+		midden = o.getMiddenveldersKrachten();
+		verdediging = o.getVerdedigersKrachten();
 	}
 	
 	public Team getTeam() {
@@ -24,4 +37,54 @@ public class WedstrijdTeam {
 	public void maakDoelpunt() {
 		++score;
 	}
+	
+	public SomKrachten getAanval() {
+		return aanval;
+	}
+	
+	public SomKrachten getMiddenveld() {
+		return midden;
+	}
+	
+	public SomKrachten getVerdediging() {
+		return verdediging;
+	}
+	
+	public boolean quickEquals(WedstrijdTeam b) {
+		return this.x == b.x;
+	}
+	
+	public float getKracht(int veld, boolean aanvallend) {
+		float pow = 0;
+		
+		switch(veld) {
+		case 1:
+			// Team A doelgebied
+			if(aanvallend) {
+				pow += verdediging.getAanval();
+			} else {
+				pow += verdediging.getVerdediging();
+			}
+			break;
+		case 2:
+			// Tussen midden en doelgebied A in.
+			
+			break;
+		case 3:
+			// Midden
+			
+			break;
+		case 4:
+			// Tussen midden en doelgebied B in.
+			
+			break;
+		case 5:
+			// Team B doelgebied
+			
+			break;
+		}
+		
+		return pow;
+	}
+	
 }
