68c73a510ff259da91fe1d25b777397c990aa9ea
8uurg <arthur.guijt12@gmail.com>
2015-01-18 16:35:17 +0100

    Sla gegevens op voor klassement. Todo: zorg ervoor dat de getallen in Team correct ingesteld worden.

--- src/voetbalmanager/controller/wedstrijdsimulatie/Resultaat.java
+++ src/voetbalmanager/controller/wedstrijdsimulatie/Resultaat.java
@@ -1,12 +1,16 @@
 package voetbalmanager.controller.wedstrijdsimulatie;
 
 public class Resultaat {
-	int scoreA;
-	int scoreB;
+	private int scoreA;
+	private int scoreB;
 	
 	public Resultaat(int scoreA, int scoreB) {
 		this.scoreA = scoreA;
 		this.scoreB = scoreB;
+	}
+	
+	public int[] getScore() {
+		return new int[]{scoreA, scoreB};
 	}
 	
 	public String toString() {
