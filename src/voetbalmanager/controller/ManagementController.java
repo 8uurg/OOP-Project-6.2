0d6e5763ffd5a6bf9a0bd90abfe4c9de54614191
Ravi Autar <ravi_autar27@hotmail.com>
2015-01-24 18:14:46 +0100

    backgrounds gefixed, loadgame ook netjes gemaakt, popups verwijdert.. bezig met de uml

--- src/voetbalmanager/controller/ManagementController.java
+++ src/voetbalmanager/controller/ManagementController.java
@@ -87,7 +87,7 @@ public class ManagementController implements Initializable, ControlledScreen,
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// disable editing op de textarea
-		//Budget.setDisable(true);
+		Budget.setDisable(true);
 
 		// TODO speelschema inladen
 		Club.setCellValueFactory(
@@ -130,50 +130,18 @@ public class ManagementController implements Initializable, ControlledScreen,
 	@FXML
 	public void handleOpslaan() throws IOException {
 		XMLWriter.saveCompetitie(Main.huidigSpel.getCompetitie());
-		Parent root = FXMLLoader.load(Main.class
+		/*Parent root = FXMLLoader.load(Main.class
 				.getResource("view/LoadPopup.fxml"));
 		Scene scene = new Scene(root);
 		stage.setScene(scene);
 		stage.setTitle("Save succesfull");
-		stage.show();
+		stage.show();*/
 	}
 
 	@FXML
 	public void handleExit() throws IOException {
-		Parent root = FXMLLoader.load(Main.class
-				.getResource("view/exitPopup.fxml"));
-		Scene scene = new Scene(root);
-		Stage stage = new Stage();
-		stage.setScene(scene);
-		stage.setTitle("Save?");
-		stage.show();
 		myController.setScreen(Main.MainMenu);
 
-	}
-
-	@FXML
-	public void handleLoadPopup() {
-		Stage st = (Stage) closeButton.getScene().getWindow();
-		st.close();
-	}
-
-	@FXML
-	public void jaExitPopup() throws IOException{
-		//XMLWriter.saveCompetitie(Main.huidigSpel.getCompetitie());
-		Stage st = (Stage) oplaan.getScene().getWindow();
-		st.close();
-		Parent root = FXMLLoader.load(Main.class
-				.getResource("view/LoadPopup.fxml"));
-		Scene scene = new Scene(root);
-		Stage stage = new Stage();
-		stage.setScene(scene);
-		stage.setTitle("Save?");
-		stage.show();
-	}
-	@FXML
-	public void neeExitPopup(){
-		Stage st = (Stage) terug.getScene().getWindow();
-		st.close();
 	}
 
 	@Override
